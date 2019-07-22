24c92dd0e7ceafc33756d71d244da006739c2788
Erik Grubbs <erikgrubbs@gmail.com>
2019-07-22 15:51:33 -0700

    [feat] Implements all three components

--- server/router.js
+++ server/router.js
@@ -5,17 +5,37 @@ router.route('/search')
   .all((req, res) => {
     res.redirect("http://localhost:2001/api" + req.url);
   });
- 
-router.route('/viewer')
+
+router.route('/viewer/products')
   .all((req, res) => {
+    console.log(req.url);
     res.redirect("http://localhost:2002/api" + req.url);
-  });  
+  });
+
+router.route('/product/:id')
+.all((req, res) => {
+    res.redirect("http://localhost:2003/api" + req.url)
+  });
+
+router.route('/text/:id')
+  .all((req, res) => {
+    res.redirect("http://localhost:2003/api" + req.url)
+  });
+
+router.route('/qanda/:id')
+  .all((req, res) => {
+    res.redirect("http://localhost:2003/api" + req.url)
+  });
+
+router.route('/gallery/:id')
+  .all((req, res) => {
+    res.redirect("http://localhost:2003/api" + req.url)
+  });
 
 
-  
 
-    
-  
+
+
 
 
 module.exports = router;
